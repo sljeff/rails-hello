91ff23295b973f74e8b3afb1b0460626c6b894e2
sljeff <shanliangjeff@qq.com>
2017-05-16 23:14:37 +0800

    rails教程第二章完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html: 'Hello, Jeff!'
-  end
-
-  def goodbye
-    render html: 'Good Bye! World!'
-  end
 end
