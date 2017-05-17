3393db770f9479846a49cb91c2e55ef63244d783
sljeff <shanliangjeff@qq.com>
2017-05-17 10:34:08 +0800

    第三章完成

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @main_title = 'HelloApp'
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select 'title', "Home - #{@main_title}"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select 'title', "Help - #{@main_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select 'title', "About - #{@main_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select 'title', "Contact - #{@main_title}"
+  end
+end
